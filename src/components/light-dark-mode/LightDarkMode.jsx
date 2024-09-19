fb439f39f1ac3ae4d344be4ce97e5e527b3f3265
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 01:58:56 +0200

    temp add uselocalstorage custom hook

--- src/components/light-dark-mode/LightDarkMode.jsx
+++ src/components/light-dark-mode/LightDarkMode.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 import './styles.css';
 
 export const LightDarkMode = () => {
-  return <div className='test'>LightDarkMode</div>;
+  return (
+    <div className='light-dark-mode'>
+      <div className='container'>
+        <h1>Light and Dark Mode</h1>
+        <button>Change Theme</button>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/light-dark-mode/UseLocalStorage.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+export const UseLocalStorage = (key, defaultValue) => {
+  const [value, setValue] = useState(() => {
+    let currentValue;
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(defaultValue)
+      );
+    } catch (error) {
+      currentValue = defaultValue;
+    }
+    return currentValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
