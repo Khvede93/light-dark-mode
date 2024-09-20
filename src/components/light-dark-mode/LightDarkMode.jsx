4919c5a3666c84d5c9084c3e213dcde6974856e9
Khvede93 <gio.khvede93@gmail.com>
2024-09-20 02:04:10 +0200

    working version of component without css is done

--- src/components/light-dark-mode/LightDarkMode.jsx
+++ src/components/light-dark-mode/LightDarkMode.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './styles.css';
+import { UseLocalStorage } from './UseLocalStorage';
 
 export const LightDarkMode = () => {
+  const [theme, setTheme] = UseLocalStorage('theme', 'dark');
+
+  function handleToggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
   return (
     <div className='light-dark-mode'>
       <div className='container'>
         <h1>Light and Dark Mode</h1>
-        <button>Change Theme</button>
+        <button onClick={handleToggleTheme}>
+          Change To {theme === 'light' ? 'Dark' : 'Light'}
+        </button>
       </div>
     </div>
   );
